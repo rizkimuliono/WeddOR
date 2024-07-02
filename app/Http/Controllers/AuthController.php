a981d29c19fb06649099566f46613781070dc048
rizkimuliono <rizkimuliono@gmail.com>
2024-07-03 00:20:17 +0700

    auth to Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     public function login(Request $request)
